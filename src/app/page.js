470fc853c2a5e13ac12bc7b7c41d253c763dbfaa
Kelvin Chen <kelvin.c.chen@gmail.com>
2024-12-13 13:16:15 -0800

    tabs in vanilla and react

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import './page.module.css';
 import { MortgageCalculator } from './mortgage.js';
 import { Hypergeo } from './karsten.js';
+import { Tabs } from './tabs.js';
 
 // this fucking react project is 350 MB... gonna reuse it for react practice
 
 export default function Home() {
   // return <MortgageCalculator />
-  return <Hypergeo />
+  // return <Hypergeo />
+  return <Tabs />
 }
