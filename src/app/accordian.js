5eab3b25ae57916268018228bf1d4c6e3ad5f37a
Kelvin Chen <kelvin.c.chen@gmail.com>
2024-12-14 13:56:14 -0800

    accordian

---
+++ src/app/accordian.js
@@ -0,0 +1,56 @@
+'use client';
+import { useState } from 'react';
+
+export function Accordian() {
+
+  function Section({ title, body }) {
+    const [isCollapsed, setCollapsed] = useState(true);
+
+    function handleClick() {
+      setCollapsed(!isCollapsed);
+    }
+
+    return (
+      <article>
+        <div>
+          {title}
+          <button onClick={handleClick}>
+            <span
+              aria-hidden="true"
+              className={isCollapsed ? 'accordion-icon' : 'accordion-icon rotated'}></span>
+          </button>
+        </div>
+        <p hidden={isCollapsed}>
+          {body}
+        </p>
+      </article>
+    )
+  }
+
+  const htmlBody = `
+  The HyperText Markup Language or HTML is the
+  standard markup language for documents designed to
+  be displayed in a web browser.
+  `;
+
+  const cssBody = `
+  Cascading Style Sheets is a style sheet language
+  used for describing the presentation of a document
+  written in a markup language such as HTML or XML.
+  `;
+
+  const jsBody = `
+  JavaScript, often abbreviated as JS, is a
+  programming language that is one of the core
+  technologies of the World Wide Web, alongside HTML
+  and CSS.
+  `;
+
+  return (
+    <article>
+      <Section title="HTML" body={htmlBody} />
+      <Section title="CSS" body={cssBody} />
+      <Section title="Javascript" body={jsBody} />
+    </article>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import './page.module.css';
 import { MortgageCalculator } from './mortgage.js';
 import { Hypergeo } from './karsten.js';
 import { Tabs } from './tabs.js';
+import { Accordian } from './accordian';
 
 // this fucking react project is 350 MB... gonna reuse it for react practice
 
 export default function Home() {
   // return <MortgageCalculator />
   // return <Hypergeo />
-  return <Tabs />
+  // return <Tabs />
+  return <Accordian />
 }
