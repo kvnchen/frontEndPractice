1d7d3517015045bf5660892ffc900bd43e9531ab
Kelvin Chen <kelvin.c.chen@gmail.com>
2025-05-08 10:00:33 -0700

    accordian keyboard support

--- src/app/accordian/accordian.js
+++ src/app/accordian/accordian.js
@@ -2,19 +2,51 @@
 import { useState } from 'react';
 import styles from './styles.module.css';
 
+/**
+ * adding keyboard support:
+ * 
+ * enter, up/down arrow, home, end
+ * 
+ * onKeyDown
+ *  event.key 
+ *    'Enter'
+ *    'ArrowUp'
+ *    'ArrowDown'
+ *    'Home'
+ *    'End'
+ */
 export function Accordian() {
-
-  function Section({ title, body }) {
+  function Section({ title, body, buttonId }) {
     const [isCollapsed, setCollapsed] = useState(true);
 
     function handleClick() {
       setCollapsed(!isCollapsed);
     }
+    
+    function handleKeypress(event) {
+      const id = Number(event.target.id);
+
+      // Enter fires onClick already
+      if (event.key === 'ArrowUp' && id > 0) {
+        document.getElementById(id - 1).focus();
+      } else if (event.key === 'ArrowDown' && id < 2) {
+        document.getElementById(id + 1).focus();
+      } else if (event.key === 'Home') {
+        document.getElementById(0).focus();
+      } else if (event.key === 'End') {
+        document.getElementById(2).focus();
+      }
+    }
 
     return (
       <div className={styles.accordian}>
         <div>
-          <button onClick={handleClick} className={styles.title}>
+          <button
+            id={buttonId}
+            onClick={handleClick} 
+            className={styles.title}
+            onKeyDown={handleKeypress}
+          >
             {title}
             <span
               aria-hidden="true"
@@ -51,9 +83,9 @@ export function Accordian() {
 
   return (
     <article>
-      <Section title="HTML" body={htmlBody} />
-      <Section title="CSS" body={cssBody} />
-      <Section title="Javascript" body={jsBody} />
+      <Section buttonId={0} title="HTML" body={htmlBody} />
+      <Section buttonId={1} title="CSS" body={cssBody} />
+      <Section buttonId={2} title="Javascript" body={jsBody} />
     </article>
   )
 }
